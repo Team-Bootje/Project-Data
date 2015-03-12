64b82b0b3d82ca7f25a1c306636d1034209fde2c
RoyvandenHeuvel <0878963@hr.nl>
2015-03-12 17:02:30 +0100

    non-streaming crawler

---
+++ TwitterAPI/src/twitterapi/TwitterAPI.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitterapi;
+
+import twitter4j.*;
+import twitter4j.conf.*;
+
+/**
+ *
+ * @author Roy van den Heuvel
+ */
+public class TwitterAPI {
+
+    public static void timeline() throws TwitterException {
+        Twitter twitter = TwitterFactory.getSingleton();
+        Query query = new Query("Rotterdam");
+        query.setCount(100);
+        QueryResult result = twitter.search(query);
+        for (Status status : result.getTweets()) {
+            System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+        }
+    }
+
+    public static void main(String[] args) throws TwitterException{
+        timeline();
+    }
+}
--- TwitterStreamer/src/twitterstreamer/TwitterStreamer.java
+++ TwitterStreamer/src/twitterstreamer/TwitterStreamer.java
@@ -1,16 +1,19 @@
 package twitterstreamer;
 
 import twitter4j.*;
+import twitter4j.conf.*;
 
 public final class TwitterStreamer {
 
     public static void main(String[] args) throws TwitterException {
-        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
-        FilterQuery fq = new FilterQuery();
-        
-        String keywords[] = {"SS Rotterdam"};
-        fq.track(keywords);
-        
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true);
+        cb.setOAuthConsumerKey("TMoLusb1KCZ2KNWHREQj1dN7s");
+        cb.setOAuthConsumerSecret("nVbHmDf1T1u7T8Wh8XVhffnlA8CbS4zsh8pa02qjHcHv3fLO4W");
+        cb.setOAuthAccessToken("356841347-O1uAf3mdaNQtTXbXDFecnERdoAtvZhXdbpUw5IRc");
+        cb.setOAuthAccessTokenSecret("9YHVKXTAoFjVS5vMfX4ZvlRUoVTDFgP7neDklWSTTNZ9e");
+        TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+     
         StatusListener listener = new StatusListener() {
             @Override
             public void onStatus(Status status) {
@@ -42,7 +45,14 @@ public final class TwitterStreamer {
                 ex.printStackTrace();
             }
         };
+
+        FilterQuery fq = new FilterQuery();
+
+        String keywords[] = {"Rotterdam"};
+
+        fq.track(keywords);
+
         twitterStream.addListener(listener);
-        twitterStream.sample();
+        twitterStream.filter(fq);
     }
 }
