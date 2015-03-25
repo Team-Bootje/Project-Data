084272af7bd5c1fe86b6d369389f013ebdbe65d5
RoyvandenHeuvel <0878963@hr.nl>
2015-03-25 12:42:11 +0100

    db connectie

--- TwitterAPI/src/twitterapi/TwitterAPI.java
+++ TwitterAPI/src/twitterapi/TwitterAPI.java
@@ -8,43 +8,67 @@ package twitterapi;
 import java.sql.*;
 import twitter4j.*;
 
-
 /**
  * @author Roy van den Heuvel
  */
 public class TwitterAPI {
 
-    public static void timeline() throws TwitterException {
+    // Database URL
+    static final String DB_URL = "jdbc:mysql://localhost/teambootje";
+
+    // Database inloggegevens
+    static final String USER = "root";
+    static final String PASS = "";
+
+    public static void timeline() throws TwitterException, SQLException {
         Twitter twitter = TwitterFactory.getSingleton();
         Query query = new Query("ssrotterdam");
         query.setCount(100);
-        /**** setSince kan alleen tot 7 dagen terug worden gebruikt****/
-        query.setSince("2015-03-12");
-        query.setUntil("2015-03-13");
+        /**
+         * ** setSince kan alleen tot 7 dagen terug worden gebruikt***
+         */
+
+     
+      
+        String post;
         
         QueryResult result = twitter.search(query);
         for (Status status : result.getTweets()) {
             String location = null;
-            if(status.getPlace() != null) {
+            if (status.getPlace() != null) {
                 location = status.getPlace().getName();
-            }            
-            System.out.println("@" + status.getUser().getScreenName() + ": " + status.getText() + " : Favorites: " + status.getFavoriteCount() + " : Retweets: " + status.getRetweetCount() + " : Gepost vanuit: " + location);            
+            } else {
+                location = "Niet beschikbaar";
+            }
+            System.out.println("@" + status.getUser().getScreenName() + ": " + status.getText() + " : Favorites: " + status.getFavoriteCount() + " : Retweets: " + status.getRetweetCount() + " : Gepost vanuit: " + location);
+            Connection conn = getConn();
+            String SQLQuery = " insert into posts (PID, DID, AID, Post)"
+                    + " values(?, ?, ?, ?)";
+            PreparedStatement preparedStmnt = conn.prepareStatement(SQLQuery);
+
+            post = status.getText();
+            preparedStmnt.setInt(1, 1);
+            preparedStmnt.setInt(2, 2);
+            preparedStmnt.setInt(3, 3);
+            preparedStmnt.setString(4, post);     
+            preparedStmnt.execute();
         }
     }
-
-    public static void main(String[] args) throws TwitterException {
-        try {
-            Connection conn = null;
+    
+    public static Connection getConn() {
+        Connection conn = null;
+        try {          
             // Step 1: Load the JDBC driver. 
-            Class.forName("org.gjt.mm.mysql.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             // Step 2: Establish the connection to the database. 
-            String url = "jdbc:mysql://145.24.235.14/teambootje";
-            String user = "root";
-            String password = "";
-            conn = DriverManager.getConnection(url, user, password);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
         } catch (Exception e) {
             System.err.println("Got an exception!");
         }
+        return conn;
+    }
+
+    public static void main(String[] args) throws TwitterException, SQLException {
         timeline();
     }
 }
