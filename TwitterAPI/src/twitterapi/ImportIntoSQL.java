ab42ba75d1960feb6ef1a858ab3e698b99793292
RoyvandenHeuvel <0878963@hr.nl>
2015-04-02 18:39:00 +0200

    Database integratie werkt

--- TwitterAPI/src/twitterapi/ImportIntoSQL.java
+++ TwitterAPI/src/twitterapi/ImportIntoSQL.java
@@ -20,41 +20,59 @@ public class ImportIntoSQL {
         String post = TwitterAPI.getPost();
         Date date = TwitterAPI.getDate();
 
-        // LOCATIE
-        if (locationCountry != null && !locationCity.isEmpty()) {
-            PreparedStatement locatieStmnt = conn.prepareStatement("INSERT INTO locatie(Land, Stad) VALUES(?, ?);");
+        // Check of locatie al bestaat, indien niet, insert locatie
+        String select_lid_from_locatie_where_land___and_ = "select lid from locatie where land = ? and stad = ?";
+        PreparedStatement locatiePrep = conn.prepareStatement(select_lid_from_locatie_where_land___and_);
+        locatiePrep.setString(1, locationCountry);
+        locatiePrep.setString(2, locationCity);
+        Integer locatieId = getIdOrNull(locatiePrep);
+        if (locatieId == null) {
+            // doe een insert
+            if (locationCountry != null && !locationCountry.isEmpty()) {
+                PreparedStatement locatieStmnt = conn.prepareStatement("INSERT INTO locatie(Land, Stad) VALUES(?, ?);");
 
-            if (locationCity != null && !locationCity.isEmpty()) {
+                String tweedeColom = locationCity != null && !locationCity.isEmpty()
+                        ? locationCity
+                        : null;
+
                 locatieStmnt.setString(2, locationCity);
+
+                locatieStmnt.setString(1, locationCountry);
+                locatieStmnt.execute();
             }
-            locatieStmnt.setString(1, locationCountry);
-            locatieStmnt.execute();
+
+            locatieId = getIdOrNull(locatiePrep);
         }
 
-        // PERSOON
-        PreparedStatement persoonStmnt = conn.prepareStatement("INSERT INTO persoon(Name, LID) VALUES(?, ?);");
-        PreparedStatement checkLID = conn.prepareStatement("SELECT LID FROM locatie WHERE stad = ?");
+        // Check of persoon al bestaat, indien niet, insert persoon
+        String persoonSelect = "select aid from persoon where name = ?";
+        PreparedStatement persoonPrep = conn.prepareStatement(persoonSelect);
+        persoonPrep.setString(1, screenName);
+        Integer persoonId = getIdOrNull(persoonPrep);
+        if (persoonId == null) {
+            // doe een insert
+            PreparedStatement locatieStmnt = conn.prepareStatement("INSERT INTO persoon(name, lid) VALUES(?, ?);");
+            locatieStmnt.setString(1, screenName);
+            locatieStmnt.setObject(2, locatieId);
+            locatieStmnt.execute();
 
-        // Haalt LID van persoon op.
-        checkLID.setString(1, locationCity);
-        ResultSet RSCheckLID = checkLID.executeQuery();
-
-        while (RSCheckLID.next()) {
-            int LID = RSCheckLID.getInt(1);
-            System.out.println("==========" + LID + "==========");
-
-            // Maakt de query af en voert de query uit.
-            persoonStmnt.setString(1, screenName);
-            persoonStmnt.setInt(2, LID);
-            persoonStmnt.execute();
-
+            persoonId = getIdOrNull(persoonPrep);
         }
 
         // POSTS
-        PreparedStatement postStmnt = conn.prepareStatement("INSERT INTO posts(Post, Datum) VALUES(?, ?);");
-        //postStmnt.setInt(1, x);
-        postStmnt.setString(1, post);
-        postStmnt.setDate(2, date);
+        PreparedStatement postStmnt = conn.prepareStatement("INSERT INTO posts(AID, Post, Datum) VALUES(?, ?, ?);");
+
+        postStmnt.setInt(1, persoonId);
+        postStmnt.setString(2, post);
+        postStmnt.setDate(3, date);
         postStmnt.execute();
+    }
+
+    private static Integer getIdOrNull(PreparedStatement locatiePrep) throws SQLException {
+        ResultSet locatieResultSet = locatiePrep.executeQuery();
+        if (locatieResultSet.next()) {
+            return locatieResultSet.getInt(1);
+        }
+        return null;
     }
 }
--- TwitterAPI/src/twitterapi/TwitterAPI.java
+++ TwitterAPI/src/twitterapi/TwitterAPI.java
@@ -19,17 +19,16 @@ public class TwitterAPI {
     // Database inloggegevens
     static final String USER = "root";
     static final String PASS = "";
-    
+
     //variabelen
     static String cityVar;
     static String countryVar;
     static String screenName;
     static String post;
     static Date date;
-    
 
     public static void timeline() throws TwitterException, SQLException {
-        Twitter twitter = TwitterFactory.getSingleton();      
+        Twitter twitter = TwitterFactory.getSingleton();
         Query query = new Query("ssrotterdam");
         query.setCount(100);
         /**
@@ -46,31 +45,30 @@ public class TwitterAPI {
                 locationCity = status.getPlace().getName();
                 locationCountry = status.getPlace().getCountry();
             } else {
-                countryVar = "N/A";
-                cityVar = "N/A";
-                locationCity = "N/A";
-                locationCountry = "N/A";
+                countryVar = null;
+                cityVar = null;
+                locationCity = null;
+                locationCountry = null;
             }
             //System.out.println("@" + status.getUser().getScreenName() + ": " + status.getText() + " : Favorites: " + status.getFavoriteCount() + " : Retweets: " + status.getRetweetCount() + " : Gepost vanuit: " + locationCity);         
             // Converteert de datum vanuit de twitter API naar een datum object dat SQL kan gebruiken.
             java.util.Date utilDate = status.getCreatedAt();
             java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
-            
+
             date = sqlDate;
             post = status.getText();
             screenName = status.getUser().getScreenName();
-            try{
+            try {
                 ImportIntoSQL.TwitterImport();
-            }
-            catch(SQLException e){
-                //e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
     }
 
     public static Connection getConn() {
         Connection conn = null;
-        try { 
+        try {
             // Step 1: Load the JDBC driver. 
             Class.forName("com.mysql.jdbc.Driver");
             // Step 2: Establish the connection to the database. 
@@ -80,24 +78,24 @@ public class TwitterAPI {
         }
         return conn;
     }
-    
-    public static String getCityVar(){
+
+    public static String getCityVar() {
         return cityVar;
     }
-    
-    public static String getCountryVar(){
+
+    public static String getCountryVar() {
         return countryVar;
     }
-    
-    public static String getScreenName(){
+
+    public static String getScreenName() {
         return screenName;
     }
-    
-    public static String getPost(){
+
+    public static String getPost() {
         return post;
     }
-    
-    public static Date getDate(){
+
+    public static Date getDate() {
         return date;
     }
 
